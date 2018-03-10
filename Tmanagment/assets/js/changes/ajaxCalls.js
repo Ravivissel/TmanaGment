b3dc006b6a2c7472295bef9578b0fb7a06edb65a
raviv <raviv.vissel1@gmail.com>
2018-03-10 14:37:16 +0200

    Qjery is creating the datatable using add row function - static

--- Tmanagment/assets/js/changes/ajaxCalls.js
+++ Tmanagment/assets/js/changes/ajaxCalls.js
@@ -45,3 +45,19 @@ function getOpenedProjects(request, successCB, errorCB) {
         error: errorCB
     }); // end of ajax call
 }
+
+function getAllOpenTasks(request, successCB, errorCB) {
+
+    // serialize the object to JSON string
+    var dataString = JSON.stringify(request);
+
+    $.ajax({ // ajax call starts
+        url: 'ProjectWS.asmx/GetAllOpenTasksList',       // server side web service method
+        data: dataString,                          // the parameters sent to the server
+        type: 'POST',                              // can be also GET
+        dataType: 'json',                          // expecting JSON datatype from the server
+        contentType: 'application/json; charset = utf-8', // sent to the server
+        success: successCB,                // data.d id the Variable data contains the data we get from serverside
+        error: errorCB
+    }); // end of ajax call
+}
--- Tmanagment/assets/js/changes/tasksPage.js
+++ Tmanagment/assets/js/changes/tasksPage.js
@@ -2,7 +2,22 @@
     $(document).ready(function() {
 
         // Default Datatable
-        $('#datatable').DataTable();
+      var t = $('#datatable-buttons').DataTable();
+
+        var counter = 1;
+        t.row.add( [
+            counter +'.1',
+            counter +'.2',
+            counter +'.3',
+            counter +'.4',
+            counter +'.5',
+            counter +'.6',
+            counter +'.7',
+            counter +'.8'
+        ] ).draw( false );
+
+        counter++;
+
 
         //Buttons examples
         var table = $('#datatable-buttons').DataTable({
@@ -25,22 +40,6 @@
                 style: 'multi'
             }
         });
-        var counter = 1;
 
-        $('#datatable').on( 'hover', function () {
-            t.row.add( [
-                counter +'.1',
-                counter +'.2',
-                counter +'.3',
-                counter +'.4',
-                counter +'.5'
-            ] ).draw( false );
 
-            counter++;
-        } );
-
-        // Automatically add a first row of data
-        $('#datatable').click();
-        table.buttons().container()
-            .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
     } );
