0ec4d5ea645ecd10b3ac6e9f088cfa898a2d6596
Raviv.v <raviv.vissel1@gmail.com>
2018-03-20 11:36:21 -0700

    Add new task and new request are working

--- Tmanagment/assets/js/changes/utils.js
+++ Tmanagment/assets/js/changes/utils.js
@@ -4,17 +4,16 @@ $(document).ready(function () {
 
 });
 
-$(document).on('click', '#newRequestsForm', function () {
-    requestID = -1;
-    arr_details = { requestID: requestID, func: "new" };
+$(document).on('click', '#newRequestForm', function () {
+    var requestID = -1;
+    var arr_details = { requestID: requestID, func: "new" };
     localStorage.requestList = JSON.stringify(arr_details);
-    //location.href = "requestsForm.html";
+    location.href = "requestsForm.html";
 });
-
 $(document).on('click', '#newTaskForm', function () {
-    taskID = -1;
-    arr_details = { taskID: taskID, func: "new" };
+    var taskID = -1;
+    var arr_details = { taskID: taskID, func: "new" };
     localStorage.taskList = JSON.stringify(arr_details);
+    location.href = "taskForm.html";
 });
 
- 
